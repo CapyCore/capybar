b3571605b7f061ac68f2745699deb7f563fbde8b
pepengu <plotnikovdanya@gmail.com>
2025-07-07 21:19:13 +0300

    feat: improved color + tests

--- src/util/color.rs
+++ src/util/color.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 use serde::Deserialize;
 
 /// Color structure used in capy. Color is stored as an rgba value.
-#[derive(Clone, Copy, Debug, Default, Deserialize)]
+#[derive(Clone, Copy, Debug, Default, Deserialize, PartialEq)]
 pub struct Color(u32);
 
 impl Display for Color {
@@ -41,12 +41,60 @@ impl Color {
         Self(u32::from_le_bytes(*bytes))
     }
 
+    pub fn from_rgba_f32(r: f32, g: f32, b: f32, a: f32) -> Option<Color> {
+        if !(0.0..=1.0).contains(&r)
+            || !(0.0..=1.0).contains(&g)
+            || !(0.0..=1.0).contains(&b)
+            || !(0.0..=1.0).contains(&a)
+        {
+            return None;
+        }
+
+        Some(Self::from_rgba(
+            (r * 255.0).round() as u8,
+            (g * 255.0).round() as u8,
+            (b * 255.0).round() as u8,
+            (a * 255.0).round() as u8,
+        ))
+    }
+
     pub const fn to_be_bytes(self) -> [u8; 4] {
         self.0.to_be_bytes()
     }
 
     pub const fn to_le_bytes(self) -> [u8; 4] {
         self.0.to_le_bytes()
+    }
+
+    pub fn r(&self) -> u8 {
+        ((self.0 & 0xFF000000) >> 24) as u8
+    }
+
+    pub fn g(&self) -> u8 {
+        ((self.0 & 0x00FF0000) >> 16) as u8
+    }
+
+    pub fn b(&self) -> u8 {
+        ((self.0 & 0x0000FF00) >> 8) as u8
+    }
+
+    pub fn a(&self) -> u8 {
+        (self.0 & 0x000000FF) as u8
+    }
+
+    pub fn set_r(&mut self, a: u8) {
+        self.0 &= 0x00FFFFFF;
+        self.0 |= (a as u32) << 24;
+    }
+
+    pub fn set_g(&mut self, a: u8) {
+        self.0 &= 0xFF00FFFF;
+        self.0 |= (a as u32) << 16;
+    }
+
+    pub fn set_b(&mut self, a: u8) {
+        self.0 &= 0xFFFF00FF;
+        self.0 |= (a as u32) << 8;
     }
 
     pub fn set_a(&mut self, a: u8) {
@@ -58,27 +106,32 @@ impl Color {
         let bg = background.to_be_bytes();
         let fg = foreground.to_be_bytes();
 
-        //TODO check if checking for a == 0 improves speed
+        if fg[3] == 0 {
+            return *background;
+        }
+        if fg[3] == 255 {
+            return *foreground;
+        }
+        if bg[3] == 0 {
+            return *foreground;
+        }
 
-        let bg = [
-            bg[0] as f32 * bg[3] as f32,
-            bg[1] as f32 * bg[3] as f32,
-            bg[2] as f32 * bg[3] as f32,
-            bg[3] as f32,
-        ];
-        let fg = [
-            fg[0] as f32 * fg[3] as f32,
-            fg[1] as f32 * fg[3] as f32,
-            fg[2] as f32 * fg[3] as f32,
-            fg[3] as f32,
-        ];
+        let bg_alpha = bg[3] as f32 / 255.0;
+        let fg_alpha = fg[3] as f32 / 255.0;
 
-        let coef = 1.0 - fg[3] / 255.0;
-        let a = fg[3] + bg[3] * coef;
+        let a = fg_alpha + bg_alpha * (1.0 - fg_alpha);
+
+        let blend_channel = |fg_c: u8, bg_c: u8| -> u8 {
+            let fg_norm = fg_c as f32 / 255.0;
+            let bg_norm = bg_c as f32 / 255.0;
+            let blended = (fg_norm * fg_alpha) + (bg_norm * bg_alpha * (1.0 - fg_alpha));
+            (blended / a * 255.0).round() as u8
+        };
+
         Color::from_rgba(
-            ((fg[0] + bg[0] * coef) / a).floor() as u8,
-            ((fg[1] + bg[1] * coef) / a).floor() as u8,
-            ((fg[2] + bg[2] * coef) / a).floor() as u8,
+            blend_channel(fg[0], bg[0]),
+            blend_channel(fg[1], bg[1]),
+            blend_channel(fg[2], bg[2]),
             (a * 255.0).floor() as u8,
         )
     }
